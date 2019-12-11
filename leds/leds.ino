eb2e3e6b4a748a405f93945c10e4ec08a92def8c
Welbert Hime <welberthime@hotmail.com>
2019-12-10 22:18:52 -0300

    Adicionando projeto do segundo arduino

--- leds/leds.ino
+++ leds/leds.ino
@@ -2,74 +2,94 @@
 
 int pino6 = 6;
 int button1 = 2;
-int mov;
-volatile int escala_atual = 0;
-char posicao_nota = '0';
+int posicao_nota;
+int receber0 = A0;
+int receber1 = A1;
+int receber2 = A2;
+int escala_atual = 0;
+int controle = 0;
 
+// initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 8);
 
 void setup(){
   Serial.begin(9600);
-  lcd.begin(16, 2);
   pinMode(pino6, OUTPUT);
-  pinMode(button1, INPUT);
-  attachInterrupt(digitalPinToInterrupt(button1), botaofunc, FALLING);
+  pinMode(button1, INPUT_PULLUP);
+  pinMode(A4,OUTPUT);
+  lcd.begin(16, 2);
   analogWrite(pino6, 80);
   estado(escala_atual);
 }
 
-
-void andador() {
-  lcd.setCursor(0,0);
-  lcd.print("                          ");
-  if (escala_atual == 0) {
-    if (posicao_nota == 1) {
+void andador(){
+  if(escala_atual == 0){
+    if(posicao_nota == 1){
       lcd.setCursor(0,0);
-      lcd.print("o");
-    } else if (posicao_nota == 2) {
-      lcd.setCursor(2,0);
-      lcd.print("o");
-    } else if (posicao_nota == 3) {
-      lcd.setCursor(4,0);
-      lcd.print("o");
-    } else if(posicao_nota == 4) {
-      lcd.setCursor(6,0);
-      lcd.print("o");
-    } else if (posicao_nota == 5) {
-      lcd.setCursor(8,0);
-      lcd.print("o");
-    } else if (posicao_nota == 6) {
-      lcd.setCursor(10,0);
-      lcd.print("o");
-    } else if(posicao_nota == 7) {
-      lcd.setCursor(12,0);
-      lcd.print("o");
+      lcd.print("  *             ");
+    }else
+    if(posicao_nota == 2){
+      lcd.setCursor(0,0);
+      lcd.print("    *           ");
+    }else
+    if(posicao_nota == 3){
+      lcd.setCursor(0,0);
+      lcd.print("      *         ");
+    }else
+    if(posicao_nota == 4){
+      lcd.setCursor(0,0);
+      lcd.print("        *       ");
+    }else
+    if(posicao_nota == 5){
+      lcd.setCursor(0,0);
+      lcd.print("          *     ");
+    }else
+    if(posicao_nota == 6){
+      lcd.setCursor(0,0);
+      lcd.print("            *   ");
+    }else
+    if(posicao_nota == 7){
+      lcd.setCursor(0,0);
+      lcd.print("              * ");
+    } else {
+      lcd.setCursor(0,0);
+      lcd.print("*               ");
     }
-  } else {
-    if (posicao_nota == 3) {
+  } else{
+    if(posicao_nota == 0){
       lcd.setCursor(0,0);
-      lcd.print("o");
-    } else if (posicao_nota == 4) {
-      lcd.setCursor(2,0);
-      lcd.print("o");
-    } else if (posicao_nota == 5) {
-      lcd.setCursor(4,0);
-      lcd.print("o");
-    } else if(posicao_nota == 6) {
-      lcd.setCursor(6,0);
-      lcd.print("o");
-    } else if(posicao_nota == 7) {
-      lcd.setCursor(8,0);
-      lcd.print("o");
+      lcd.print("*               ");
+    }else
+    if(posicao_nota == 1){
+      lcd.setCursor(0,0);
+      lcd.print("  *             ");
+
+    }else
+    if(posicao_nota == 2){
+      lcd.setCursor(0,0);
+      lcd.print("    *           ");
+
+    }else
+    if(posicao_nota == 3){
+      lcd.setCursor(0,0);
+      lcd.print("      *         ");
+
+    }else
+    if(posicao_nota == 4){
+      lcd.setCursor(0,0);
+      lcd.print("        *       ");
+    } else {
+      lcd.setCursor(0,0);
+      lcd.print("*               ");
     }
   }
 }
 
-void estado(int escala) {
+void estado(int escala){
   lcd.setCursor(0,1);
-  lcd.print("                          ");
-  switch (escala) {
-    case 0:
+  lcd.print("                         ");
+  switch (escala){
+    case 0: //linha e coluna
       lcd.setCursor(0,1);
       lcd.print("C D E F G A B");
     break;
@@ -84,14 +104,42 @@ void estado(int escala) {
   }
 }
 
-void botaofunc() {
-  escala_atual = !escala_atual;
-  estado(escala_atual);
-}
+void loop(){
+    analogWrite(A4, controle);
+    int bit0 = analogRead(receber0);
+    int bit1 = analogRead(receber1);
+    int bit2 = analogRead(receber2);
 
-void loop() {
-  if (Serial.available() > 0) {
-    posicao_nota = Serial.read();
+    if(bit0 == 0 && bit1 == 0 && bit2== 0){
+      posicao_nota = 0;
+    } else if(bit0 == 1023 && bit1 == 0 && bit2 == 0){
+      posicao_nota = 1;
+    } else if(bit0 == 0 && bit1 == 1023 && bit2 == 0){
+      posicao_nota = 2;
+    } else if(bit0 == 1023 && bit1 == 1023 && bit2 == 0){
+      posicao_nota = 3;
+    } else if(bit0 == 0 && bit1 == 0 && bit2 == 1023){
+      posicao_nota = 4;
+    } else if(bit0 == 1023 && bit1 == 0 && bit2 == 1023){
+      posicao_nota = 5;
+    } else if(bit0 == 0 && bit1 == 1023 && bit2 == 1023){
+      posicao_nota = 6;
+    } else if(bit0 == 1023 && bit1 == 1023 && bit2 == 1023){
+      posicao_nota = 7;
+    }
+
+    if (digitalRead(button1) == LOW) {
+      escala_atual = !escala_atual;
+      if (controle == 255) {
+        controle = 0;
+      } else {
+        controle = 255;
+      }
+      delay(200);
+      Serial.println("Trocou");
+      estado(escala_atual);
+    }
+
+    Serial.println(posicao_nota);
     andador();
-  }
 }
